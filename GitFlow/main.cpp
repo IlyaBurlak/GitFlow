55b8e2c821d974725ce8b75b04f31ed839df2952
Ilya Burlak <113059204+IlyaBurlak@users.noreply.github.com>
2022-10-16 21:26:18 +0300

    Initial Commit

---
+++ GitFlow/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GitFlow
+//
+//  Created by Илья Бурлак on 16.10.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
