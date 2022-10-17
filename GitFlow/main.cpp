83c076a535163aa0e3ce6fd605d06681c34a6db2
Ilya Burlak <113059204+IlyaBurlak@users.noreply.github.com>
2022-10-17 09:02:01 +0300

    Fix code

--- GitFlow/main.cpp
+++ GitFlow/main.cpp
@@ -1,8 +1,9 @@
 //
-//  main.cpp
 //  GitFlow
 //
 //  Created by Илья Бурлак on 16.10.2022.
+//
+//  my email: ilya.burlak@mail.ry
 //
 
 #include <iostream>
@@ -23,7 +24,7 @@ int main() {
 
     for(int i=1 ; i<len ; i++)
         for(int j=i;j>0 && arr[j-1] > arr[j]; j--)
-            swap( arr[j-1] , arr[j] );
+            swap( arr[j-1] , arr[j] );              // swap меняеть местами элкменты (arr[j-1] , arr[j])
 
 
 
