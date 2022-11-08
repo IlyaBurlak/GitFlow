bdaca36d5c998f5e3012d857d3d5e351470334d1
Ilya Burlak <113059204+IlyaBurlak@users.noreply.github.com>
2022-11-08 10:32:36 +0300

    New branch

--- GitFlow/main.cpp
+++ GitFlow/main.cpp
@@ -7,8 +7,42 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+//  GitFlow
+//
+//  Created by Илья Бурлак on 16.10.2022.
+//
+//  my email: ilya.burlak@mail.ry
+
+
+
+
+using namespace std;
+
+int main() {
+
+    // Инициализация переменных и заполнение массива
+    int len;
+    cout <<"Введите длину массива: ";
+    cin >> len;
+    cout <<"Введите массив: ";
+    int arr[len];
+    for (int l = 0 ; l < len ; l++ ){
+        cin >> arr[l] ;
+    }
+
+    // сортировка
+
+    for(int i=1 ; i<len ; i++)
+        for(int j=i;j>0 && arr[j-1] > arr[j]; j--)
+            swap( arr[j-1] , arr[j] );              // swap меняеть местами элкменты (arr[j-1] , arr[j])
+
+
+
+   // Выаод массива.
+    cout << "Отсортированный иассив:";
+    for (int s = 0 ; s < (len); s++){
+        cout << arr[s] << "\n" ;
+    }
+
+    return 0 ;
 }
